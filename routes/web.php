16f506310ab99ed5047a4912bab38c5fbafb229d
Queniex <queniesalbiyah@gmail.com>
2022-11-23 23:29:47 +0700

    Learning about routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// ini berguna untuk membuat rute, agar dihandle saat memasukkan alamat path berdasarkan fungsi
+Route::get('/', function () { 
+    // return view('welcome');
+    return 'Halaman Home';
+});
+
+Route::get('/about', function () { 
+    // return view('welcome');
+    return 'Halaman About';
+});
+
+Route::get('/blog', function () { 
+    // return view('welcome');
+    return 'Halaman Blog';
 });
